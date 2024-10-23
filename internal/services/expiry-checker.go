2bf39e1e7c4ee4d25951a8b56e74600e4dda57b0
Gurjot <gurjotsyd@gmail.com>
2024-10-23 12:42:19 -0400

    fix todo

--- internal/services/expiry-checker.go
+++ internal/services/expiry-checker.go
@@ -44,7 +44,7 @@ func (s *Service) checkExpiry(ctx context.Context) *types.Error {
 		}
 
 		// TODO: consider eligibility for state transition here
-		// https://github.com/babylonlabs-io/babylon-staking-indexer/pull/27#discussion_r1811688208
+		// https://github.com/babylonlabs-io/babylon-staking-indexer/issues/29
 
 		if err := s.db.UpdateBTCDelegationState(ctx, delegation.StakingTxHashHex, types.StateWithdrawable); err != nil {
 			log.Error().Err(err).Msg("Error updating BTC delegation state to withdrawable")
