a224299ad20d6bba2a893dbe40d5325418705432
Gurjot <gurjotsyd@gmail.com>
2024-10-23 16:19:35 -0400

    make qualified states

--- internal/types/state.go
+++ internal/types/state.go
@@ -1,5 +1,7 @@
 package types
 
+import bbntypes "github.com/babylonlabs-io/babylon/x/btcstaking/types"
+
 // Enum values for Delegation State
 type DelegationState string
 
@@ -15,4 +17,36 @@ const (
 
 func (s DelegationState) String() string {
 	return string(s)
+}
+
+// QualifiedStatesForCovenantQuorumReached returns the qualified current states for CovenantQuorumReached event
+func QualifiedStatesForCovenantQuorumReached(babylonState string) []DelegationState {
+	switch babylonState {
+	case bbntypes.BTCDelegationStatus_VERIFIED.String(), bbntypes.BTCDelegationStatus_ACTIVE.String():
+		return []DelegationState{StatePending}
+	default:
+		return nil
+	}
+}
+
+// QualifiedStatesForInclusionProofReceived returns the qualified current states for InclusionProofReceived event
+func QualifiedStatesForInclusionProofReceived(babylonState string) []DelegationState {
+	switch babylonState {
+	case bbntypes.BTCDelegationStatus_ACTIVE.String():
+		return []DelegationState{StateVerified}
+	case bbntypes.BTCDelegationStatus_PENDING.String():
+		return []DelegationState{StatePending}
+	default:
+		return nil
+	}
+}
+
+// QualifiedStatesForUnbondedEarly returns the qualified current states for UnbondedEarly event
+func QualifiedStatesForUnbondedEarly() []DelegationState {
+	return []DelegationState{StateActive}
+}
+
+// QualifiedStatesForExpired returns the qualified current states for Expired event
+func QualifiedStatesForExpired() []DelegationState {
+	return []DelegationState{StateActive}
 }
