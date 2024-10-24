91719c6af55931441c7aa9922ded7244bbbf4676
Gurjot <gurjotsyd@gmail.com>
2024-10-24 13:16:53 -0400

    use mngr from queue client

---
+++ consumer/event_consumer.go
@@ -0,0 +1,16 @@
+package consumer
+
+import (
+	"github.com/babylonlabs-io/staking-queue-client/client"
+)
+
+type EventConsumer interface {
+	Start() error
+	PushStakingEvent(ev *client.ActiveStakingEvent) error
+	PushUnbondingEvent(ev *client.UnbondingStakingEvent) error
+	PushWithdrawEvent(ev *client.WithdrawStakingEvent) error
+	PushExpiryEvent(ev *client.ExpiredStakingEvent) error
+	PushBtcInfoEvent(ev *client.BtcInfoEvent) error
+	PushConfirmedInfoEvent(ev *client.ConfirmedInfoEvent) error
+	Stop() error
+}
