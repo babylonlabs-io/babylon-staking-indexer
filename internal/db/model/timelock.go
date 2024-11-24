d08dd7f420cf18cad6b4c7445eaebdf2a3c373f3
Gurjot <gurjotsyd@gmail.com>
2024-11-24 15:35:08 +0530

    fix timelock expiry

--- internal/db/model/timelock.go
+++ internal/db/model/timelock.go
@@ -3,17 +3,17 @@ package model
 import "github.com/babylonlabs-io/babylon-staking-indexer/internal/types"
 
 type TimeLockDocument struct {
-	StakingTxHashHex string                   `bson:"_id"` // Primary key
-	ExpireHeight     uint32                   `bson:"expire_height"`
-	SubState         types.DelegationSubState `bson:"sub_state"`
+	StakingTxHashHex   string                   `bson:"_id"` // Primary key
+	ExpireHeight       uint32                   `bson:"expire_height"`
+	DelegationSubState types.DelegationSubState `bson:"delegation_sub_state"`
 }
 
 func NewTimeLockDocument(
 	stakingTxHashHex string, expireHeight uint32, subState types.DelegationSubState,
 ) *TimeLockDocument {
 	return &TimeLockDocument{
-		StakingTxHashHex: stakingTxHashHex,
-		ExpireHeight:     expireHeight,
-		SubState:         subState,
+		StakingTxHashHex:   stakingTxHashHex,
+		ExpireHeight:       expireHeight,
+		DelegationSubState: subState,
 	}
 }
