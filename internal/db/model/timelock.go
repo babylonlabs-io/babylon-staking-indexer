8eefb802f669bbf4dd70c050d95b51af1163e336
Gurjot Singh <111540954+gusin13@users.noreply.github.com>
2024-12-20 15:34:35 +0530

    fix: multiple expiry documents for same staking tx can exist (#97)

--- internal/db/model/timelock.go
+++ internal/db/model/timelock.go
@@ -3,7 +3,7 @@ package model
 import "github.com/babylonlabs-io/babylon-staking-indexer/internal/types"
 
 type TimeLockDocument struct {
-	StakingTxHashHex   string                   `bson:"_id"` // Primary key
+	StakingTxHashHex   string                   `bson:"staking_tx_hash_hex"`
 	ExpireHeight       uint32                   `bson:"expire_height"`
 	DelegationSubState types.DelegationSubState `bson:"delegation_sub_state"`
 }
