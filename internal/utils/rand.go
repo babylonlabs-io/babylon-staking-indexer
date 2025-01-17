8ec88fff04334cfe561b93c7ca5289fcb784d963
Kirill <paltsev.kir@gmail.com>
2025-01-18 01:14:19 +0400

    Add negative length case to utils.RandAlphaNum(). Write tests for it

--- internal/utils/rand.go
+++ internal/utils/rand.go
@@ -5,8 +5,13 @@ import (
 )
 
 // RandomAlphaNum generates random alphanumeric string
+// in case length <= 0 it returns empty string
 func RandomAlphaNum(length int) string {
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	if length <= 0 {
+		return ""
+	}
 
 	randomString := make([]byte, length)
 	for i := range randomString {
