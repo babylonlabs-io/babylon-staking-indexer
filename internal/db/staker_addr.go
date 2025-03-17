51ba1694b7259850ab55df9b038eb99a4400bbdf
Kirill <paltsev.kir@gmail.com>
2025-03-17 16:32:27 +0400

    Add babylon address validation in fill-staker-addr script (#185)

--- internal/db/staker_addr.go
+++ internal/db/staker_addr.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"context"
-	"fmt"
 	"github.com/babylonlabs-io/babylon-staking-indexer/internal/db/model"
 	"go.mongodb.org/mongo-driver/bson"
 )
@@ -14,10 +13,8 @@ func (db *Database) UpdateDelegationStakerBabylonAddress(ctx context.Context, st
 			"staker_babylon_address": stakerAddr,
 		},
 	}
-	result, err := db.collection(model.BTCDelegationDetailsCollection).
+	_, err := db.collection(model.BTCDelegationDetailsCollection).
 		UpdateOne(ctx, filter, update)
-
-	fmt.Println("Result", result)
 
 	return err
 }
