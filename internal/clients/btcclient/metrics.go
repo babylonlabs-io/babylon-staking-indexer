e4467b339d5cc70f99be7733eb16f46e78889a54
Kirill <paltsev.kir@gmail.com>
2025-03-24 15:53:27 +0400

    Testing linter

--- internal/clients/btcclient/metrics.go
+++ internal/clients/btcclient/metrics.go
@@ -1,9 +1,9 @@
 package btcclient
 
 import (
-	"time"
-	"github.com/babylonlabs-io/babylon-staking-indexer/internal/observability/metrics"
 	"context"
+	"github.com/babylonlabs-io/babylon-staking-indexer/internal/observability/metrics"
+	"time"
 )
 
 type btcClientWithMetrics struct {
@@ -22,7 +22,7 @@ func (b *btcClientWithMetrics) GetTipHeight(ctx context.Context) (uint64, error)
 
 func (b *btcClientWithMetrics) GetBlockTimestamp(ctx context.Context, height uint32) (int64, error) {
 	return runBtcClientMethodWithMetrics("GetBlockTimestamp", func() (int64, error) {
-		return b.btc.GetBlockTimestamp(ctx, height)
+		return b.btc.GetBlockTimestamp(ctx, uint32(height))
 	})
 }
 
