4de709f4d4cd10b73980a8f983af9dcf93c8d700
Kirill <paltsev.kir@gmail.com>
2025-06-13 16:23:09 +0400

    Finish renaming

--- internal/db/model/bsn.go
+++ internal/db/model/bsn.go
@@ -2,48 +2,48 @@ package model
 
 import btcstkconsumer "github.com/babylonlabs-io/babylon/v4/x/btcstkconsumer/types"
 
-type EventConsumerType string
+type BSNType string
 
 const (
-	EventConsumerTypeCosmos = "cosmos"
-	EventConsumerTypeRollup = "rollup"
+	BSNTypeCosmos = "cosmos"
+	BSNTypeRollup = "rollup"
 )
 
 type BSN struct {
-	ID                     string                 `bson:"_id"`
-	Name                   string                 `bson:"name"`
-	Description            string                 `bson:"description"`
-	MaxMultiStakedFPS      uint32                 `bson:"max_multi_staked_fps"` // max number of finality providers from consumer
-	Type                   EventConsumerType      `bson:"type"`
-	RollupConsumerMetadata *ETHL2ConsumerMetadata `bson:"rollup_consumer_metadata"`
+	ID                string         `bson:"_id"`
+	Name              string         `bson:"name"`
+	Description       string         `bson:"description"`
+	MaxMultiStakedFPS uint32         `bson:"max_multi_staked_fps"` // max number of finality providers from consumer
+	Type              BSNType        `bson:"type"`
+	RollupMetadata    *ETHL2Metadata `bson:"rollup_metadata"`
 }
 
-type ETHL2ConsumerMetadata struct {
+type ETHL2Metadata struct {
 	FinalityContractAddress string `bson:"finality_contract_address"`
 }
 
 func FromEventConsumerRegistered(event *btcstkconsumer.EventConsumerRegistered) *BSN {
-	var consumerType EventConsumerType
+	var consumerType BSNType
 	switch event.ConsumerType {
 	case btcstkconsumer.ConsumerType_COSMOS:
-		consumerType = EventConsumerTypeCosmos
+		consumerType = BSNTypeCosmos
 	case btcstkconsumer.ConsumerType_ROLLUP:
-		consumerType = EventConsumerTypeRollup
+		consumerType = BSNTypeRollup
 	}
 
-	var rollupMetadata *ETHL2ConsumerMetadata
+	var rollupMetadata *ETHL2Metadata
 	if event.RollupConsumerMetadata != nil {
-		rollupMetadata = &ETHL2ConsumerMetadata{
+		rollupMetadata = &ETHL2Metadata{
 			FinalityContractAddress: event.RollupConsumerMetadata.FinalityContractAddress,
 		}
 	}
 
 	return &BSN{
-		ID:                     event.ConsumerId,
-		Name:                   event.ConsumerName,
-		Description:            event.ConsumerDescription,
-		MaxMultiStakedFPS:      event.MaxMultiStakedFps,
-		Type:                   consumerType,
-		RollupConsumerMetadata: rollupMetadata,
+		ID:                event.ConsumerId,
+		Name:              event.ConsumerName,
+		Description:       event.ConsumerDescription,
+		MaxMultiStakedFPS: event.MaxMultiStakedFps,
+		Type:              consumerType,
+		RollupMetadata:    rollupMetadata,
 	}
 }
