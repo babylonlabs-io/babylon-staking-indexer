a44914814be91bb986bf383fb8be23b2dba5875b
Kirill <paltsev.kir@gmail.com>
2025-07-21 18:37:20 +0400

    Fix maxFinalityProviders field update (#250)

--- internal/utils/poller/poller.go
+++ internal/utils/poller/poller.go
@@ -11,20 +11,19 @@ import (
 
 type Poller struct {
 	interval   time.Duration
-	quit       chan struct{}
 	pollMethod func(ctx context.Context) error
 }
 
 func NewPoller(interval time.Duration, pollMethod func(ctx context.Context) error) *Poller {
 	return &Poller{
 		interval:   interval,
-		quit:       make(chan struct{}),
 		pollMethod: pollMethod,
 	}
 }
 
 func (p *Poller) Start(ctx context.Context) {
 	ticker := time.NewTicker(p.interval)
+	defer ticker.Stop()
 
 	log := log.Ctx(ctx)
 	for {
@@ -38,14 +37,6 @@ func (p *Poller) Start(ctx context.Context) {
 			// Handle context cancellation.
 			log.Info().Msg("Poller stopped due to context cancellation")
 			return
-		case <-p.quit:
-			log.Info().Msg("Poller stopped")
-			ticker.Stop() // Stop the ticker
-			return
 		}
 	}
-}
-
-func (p *Poller) Stop() {
-	close(p.quit)
 }
