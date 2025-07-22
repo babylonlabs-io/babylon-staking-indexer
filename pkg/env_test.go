191881004181415a1a7827bdc8daa6c4900f214b
Kirill <paltsev.kir@gmail.com>
2025-07-22 05:12:26 +0400

    Add manul test (#251)
    * chore: Add manual test for bbn client

---
+++ pkg/env_test.go
@@ -0,0 +1,36 @@
+package pkg
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetenv(t *testing.T) {
+	const defaultValue = "default"
+
+	t.Cleanup(func() {
+		os.Clearenv()
+	})
+	t.Run("non existing key", func(t *testing.T) {
+		value := Getenv("non-existing-key", defaultValue)
+		assert.Equal(t, defaultValue, value)
+	})
+	t.Run("empty value used instead of default", func(t *testing.T) {
+		const key = "key"
+		err := os.Setenv(key, "")
+		require.NoError(t, err)
+		assert.Empty(t, Getenv(key, defaultValue))
+	})
+	t.Run("ok", func(t *testing.T) {
+		const (
+			key   = "key"
+			value = "value"
+		)
+		err := os.Setenv(key, value)
+		require.NoError(t, err)
+		assert.Equal(t, value, Getenv(key, defaultValue))
+	})
+}
