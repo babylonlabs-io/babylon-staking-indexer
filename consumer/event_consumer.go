e15a7e29fb9f92878e33ca70a5463a0737d5f6ca
Kirill <paltsev.kir@gmail.com>
2025-08-05 16:15:11 +0400

    Delegation expansion stats (#261)

--- consumer/event_consumer.go
+++ consumer/event_consumer.go
@@ -6,6 +6,7 @@ import (
 	"github.com/babylonlabs-io/staking-queue-client/client"
 )
 
+//go:generate mockery --name=EventConsumer --output=../tests/mocks --outpkg=mocks --filename=mock_event_consumer.go
 type EventConsumer interface {
 	Start() error
 	PushActiveStakingEvent(ctx context.Context, ev *client.StakingEvent) error
--- internal/db/model/delegation.go
+++ internal/db/model/delegation.go
@@ -62,7 +62,9 @@ type BTCDelegationDetails struct {
 	CovenantSignatures        []CovenantSignature          `bson:"covenant_unbonding_signatures"`
 	BTCDelegationCreatedBlock BTCDelegationCreatedBbnBlock `bson:"btc_delegation_created_bbn_block"`
 	SlashingTx                SlashingTx                   `bson:"slashing_tx"`
-	PreviousStakingTxHashHex  string                       `bson:"previous_staking_tx_hash_hex,omitempty"`
+	// Only expanded delegation has this field. It points to the previous staking
+	// tx hash in which the delegation was expanded. i.e this field is optional.
+	PreviousStakingTxHashHex string `bson:"previous_staking_tx_hash_hex,omitempty"`
 }
 
 func FromEventBTCDelegationCreated(
