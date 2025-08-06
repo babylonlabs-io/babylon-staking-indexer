2a1ca045755d5b3f09b287729fb65c7c5b3d7213
Kirill <paltsev.kir@gmail.com>
2025-08-06 14:37:35 +0400

    Fix parsing of event in case value of attribute starts with [ (#267)

--- internal/services/events_test.go
+++ internal/services/events_test.go
@@ -21,11 +21,35 @@ import (
 	"github.com/btcsuite/btcd/wire"
 	abcitypes "github.com/cometbft/cometbft/abci/types"
 	ctypes "github.com/cometbft/cometbft/rpc/core/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/lightningnetwork/lnd/chainntnfs"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
+
+func Test_sanitizeEvent(t *testing.T) {
+	cases := []struct {
+		name  string
+		event string
+	}{
+		{
+			name:  "Moniker value starts with [",
+			event: `{"type":"babylon.btcstaking.v1.EventFinalityProviderEdited","attributes":[{"key":"btc_pk_hex","value":"\"fc8a5b9930c3383e94bd940890e93cfcf95b2571ad50df8063b7011f120b918a\"","index":true},{"key":"commission","value":"\"0.030000000000000000\"","index":true},{"key":"details","value":"\"pSTAKE Finance is a multichain liquid staking protocol, backed by Binance Labs.\"","index":true},{"key":"identity","value":"\"CCD58C1559B694A8\"","index":true},{"key":"moniker","value":"\"[Deprecating on 10 Aug 25] pSTAKE Finance\"","index":true},{"key":"security_contact","value":"\"hello@pstake.finance\"","index":true},{"key":"website","value":"\"https://pstake.finance/\"","index":true},{"key":"msg_index","value":"0","index":true}]}`,
+		},
+	}
+
+	for _, cse := range cases {
+		var event abcitypes.Event
+		err := json.Unmarshal([]byte(cse.event), &event)
+		require.NoError(t, err)
+
+		sanitizedEvent := sanitizeEvent(event)
+
+		_, err = sdk.ParseTypedEvent(sanitizedEvent)
+		assert.NoError(t, err)
+	}
+}
 
 func TestProcessEvent(t *testing.T) {
 	t.Run("retries", func(t *testing.T) {
